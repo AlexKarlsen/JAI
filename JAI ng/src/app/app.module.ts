812ab4322fbb2f75f12b9bdbb455720afa9c6f8f
Alex Karlsen <alexkarlsen7@gmail.com>
2018-03-14 21:57:49 +0100

    intergrated with material design

--- JAI ng/src/app/app.module.ts
+++ JAI ng/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { MatGridListModule, MatCardModule } from '@angular/material';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
@@ -20,7 +22,10 @@ import { MdToHtmlPipe } from './md-to-html.pipe';
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
+    BrowserAnimationsModule,
+    MatGridListModule,
+    FlexLayoutModule,
+    MatCardModule
   ],
   providers: [
     ContentfulService
