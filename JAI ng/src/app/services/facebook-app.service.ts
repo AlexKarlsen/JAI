b86706324d3c330429cc7507fa5fa6de4a207b29
Alex Karlsen <alexkarlsen7@gmail.com>
2018-03-31 09:46:25 +0200

    removed access token

--- JAI ng/src/app/services/facebook-app.service.ts
+++ JAI ng/src/app/services/facebook-app.service.ts
@@ -20,7 +20,7 @@ export class FacebookAppService {
    * GetEvents
    */
   public GetEvents(eventId: string) {
-    this.fb.api("/JaiHaandboldAarhus/events","get", { access_token: "EAACEdEose0cBADraaC9WliAkvj4akWRjoYehBNPd9Gtt1bPE9EkbVLUjDKj6ZA0JZA7FdGmkDa5hIZA0fyAODzZB86CvZCZAXHcGJtuDzpYlDi7Iw0emkDvejbO2ZCmOICVauZBPKpesuIk5ZCwTnohHxU9zf1CJUwcLFtTuDnrSlxrufXrj7zaFFufZCST6y7ikEZD" }
+    this.fb.api("/JaiHaandboldAarhus/events","get", { access_token: "" }
     ).then(x => console.log(x))
   }
 }
